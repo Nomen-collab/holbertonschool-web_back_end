79e9172a4e339f413e215afb49495d4f17ce74e9
Nomen-collab <nomenaratsimba01@gmail.com>
2025-08-22 19:45:38 +0200

    fix task 6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,7 +1,10 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
     super(sqft);
     this._floors = floors;
   }
@@ -10,7 +13,14 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
+  set floors(newfloors) {
+    if (typeof newfloors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = newfloors;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
