d8c391ba3ded021c0e2c9eb48b952065be177b04
Nomen-collab <nomenaratsimba01@gmail.com>
2025-08-29 15:51:38 +0200

    task 0

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+/**
+ * Returns a Promise.
+ * @returns {Promise<any>} A Promise that resolves immediately.
+ */
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve();
+  });
+}
